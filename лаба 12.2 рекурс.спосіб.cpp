0a54a37f7cc01984dd179819239a170b82b2e77b
Khrystyna <hrustunakopeika2005@gmail.com>
2023-04-03 20:07:46 +0300

    first commit

---
+++ лаба 12.2 рекурс.спосіб.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* createNode(int value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertNode(Node*& head, int value) {
+    if (head == NULL) {
+        head = createNode(value);
+        return;
+    }
+    insertNode(head->next, value);
+}
+
+void insertNode(Node*& current, int value) {
+    if (current == NULL) {
+        current = createNode(value);
+        return;
+    }
+    insertNode(current->next, value);
+}
+
+void deleteNode(Node*& head, int value) {
+    if (head == NULL) {
+        return;
+    }
+    if (head->data == value) {
+        Node* tmp = head->next;
+        delete head;
+        head = tmp;
+        deleteNode(head, value);
+    }
+    else {
+        deleteNode(head->next, value);
+    }
+}
+
+int main() {
+    Node* myList = NULL;
+    insertNode(myList, 5);
+    insertNode(myList, 2);
+    insertNode(myList, 8);
+    insertNode(myList, 3);
+    cout << "Original List: ";
+    
+    deleteNode(myList, 8);
+    cout << "List after deleting 8: ";
+    
+    deleteNode(myList, 5);
+    cout << "List after deleting 5: ";
+    
+    return 0;
+}
---
+++ юніт тест 12.2_рекурс.сп.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 12.2 рекурс.спосіб/лаба 12.2 рекурс.спосіб.cpp"
+#include <cassert>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест122рекурссп
+{
+	TEST_CLASS(юніттест122рекурссп)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            Node* myList = NULL;
+            insertNode(myList, 5);
+            insertNode(myList, 2);
+            insertNode(myList, 8);
+            insertNode(myList, 3);
+            deleteNode(myList, 8);
+            Assert::AreEqual(myList->next->data, 2);
+            Assert::AreEqual(myList->next->next->data, 3);
+            deleteNode(myList, 5);
+            Assert::AreEqual(myList->data, 2);
+            Assert::AreEqual(myList->next->data, 3);
+		}
+	};
+}
